3063862b28ee18b4375f293dfc363c7ebf754049
pospi <pospi@spadgos.com>
2012-07-23 18:49:42 +1000

    fix for array-based meta values and checkbox fields

--- custom_post_type.class.php
+++ custom_post_type.class.php
@@ -820,7 +820,11 @@ class Custom_Post_Type
 
 					// set passed or default value (:WARNING: must be done after calling setQueryArgs() due to post title lookups for prefilling the list's values)
 					if (isset($meta[$metaKeyName]) && $field instanceof FormIOField_Text) {
-						$field->setValue($meta[$metaKeyName]);
+						if ($field instanceof FormIOField_Checkbox) {
+							$field->setValue(is_array($meta[$metaKeyName]) && current($meta[$metaKeyName]));
+						} else {
+							$field->setValue($meta[$metaKeyName]);
+						}
 					} else if (isset($options['default'])) {
 						$field->setValue($options['default']);
 					}
