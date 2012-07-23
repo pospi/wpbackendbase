ac90c0486db919d4349a9b8ccb9c9fdb7f8b5ae2
pospi <pospi@spadgos.com>
2012-07-23 19:22:47 +1000

    tidied up some includes and URIs
    make child element notification bubble border slightly smaller

--- pospi_base.php
+++ pospi_base.php
@@ -11,6 +11,7 @@ License: MIT
 
 define('POSPI_PLUGIN_BASE', dirname(__FILE__));
 
+// form validation & submission engine
 require_once(POSPI_PLUGIN_BASE . "/formio/form_io.class.php");
 // some additional useful form inputs
 require_once(POSPI_PLUGIN_BASE . "/field_types/formio_field-richedit.class.php");
@@ -18,19 +19,18 @@ require_once(POSPI_PLUGIN_BASE . "/field_types/formio_field-facebook_user.class.
 require_once(POSPI_PLUGIN_BASE . "/field_types/formio_field-twitter_user.class.php");
 require_once(POSPI_PLUGIN_BASE . "/field_types/formio_field-displaylink.class.php");
 require_once(POSPI_PLUGIN_BASE . "/field_types/formio_field-filesize.class.php");
-// post type inputs
+// custom post data & post type inputs
 require_once(POSPI_PLUGIN_BASE . "/custom_post_type.class.php");
 require_once(POSPI_PLUGIN_BASE . "/field_types/formio_field-posttypes.class.php");
 require_once(POSPI_PLUGIN_BASE . "/field_types/formio_field-attachments.class.php");
 require_once(POSPI_PLUGIN_BASE . "/field_types/formio_field-links.class.php");
 require_once(POSPI_PLUGIN_BASE . "/field_types/formio_field-users.class.php");
+// admin UI helper classes
+require_once(POSPI_PLUGIN_BASE . "/admin_menu.class.php");	// menu builders
+require_once(POSPI_PLUGIN_BASE . "/admin_ui.class.php");	// interface builders & custom page handlers
 
 if (is_admin()) {
 	add_action(FormIOField_Posttypes::AJAX_HOOK_NAME, 'FormIOField_Posttypes::__responseHandler');
-
-	// admin menu builder helper class
-	require_once(POSPI_PLUGIN_BASE . "/admin_menu.class.php");	// menu builders
-	require_once(POSPI_PLUGIN_BASE . "/admin_ui.class.php");	// interface builders & custom page handlers
 
 	// option to make your site's commit history visible to clients
 	define('SITE_GIT_REPO_DIR', ABSPATH . 'wp-content/');
@@ -53,15 +53,15 @@ add_action('admin_enqueue_scripts',function(){
 	wp_register_script('formio', plugins_url('formio/formio.js', __FILE__), array('jquery_tokeninput'));
 	wp_enqueue_script('formio');
 
-	wp_register_script('jcparallax', plugins_url('jc-parallax/jcparallax.js', __FILE__), array('jquery'));
+	wp_register_script('jcparallax', plugins_url('jcparallax/jcparallax.js', __FILE__), array('jquery'));
 	wp_enqueue_script('jcparallax');
-	wp_register_script('jcparallax-t', plugins_url('jc-parallax/jcp-transitioninterval.js', __FILE__), array('jquery', 'jcparallax'));
+	wp_register_script('jcparallax-t', plugins_url('jcparallax/jcp-transitioninterval.js', __FILE__), array('jquery', 'jcparallax'));
 	wp_enqueue_script('jcparallax-t');
-	wp_register_script('jcparallax-a', plugins_url('jc-parallax/jcp-animator.js', __FILE__), array('jquery', 'jcparallax-t'));
+	wp_register_script('jcparallax-a', plugins_url('jcparallax/jcp-animator.js', __FILE__), array('jquery', 'jcparallax-t'));
 	wp_enqueue_script('jcparallax-a');
-	wp_register_script('jcparallax-l', plugins_url('jc-parallax/jcp-layer.js', __FILE__), array('jquery', 'jcparallax-a'));
+	wp_register_script('jcparallax-l', plugins_url('jcparallax/jcp-layer.js', __FILE__), array('jquery', 'jcparallax-a'));
 	wp_enqueue_script('jcparallax-l');
-	wp_register_script('jcparallax-vp', plugins_url('jc-parallax/jcp-viewport.js', __FILE__), array('jquery', 'jcparallax-l'));
+	wp_register_script('jcparallax-vp', plugins_url('jcparallax/jcp-viewport.js', __FILE__), array('jquery', 'jcparallax-l'));
 	wp_enqueue_script('jcparallax-vp');
 
 	wp_register_script('pospi-admin-js', plugins_url('pospi_wp_admin.js', __FILE__), array('formio'));
@@ -79,7 +79,7 @@ add_action( 'admin_init', function() {
     wp_register_style('formio_theme_css', plugins_url('formio/themes/wordpress.css', __FILE__));
     wp_enqueue_style('formio_theme_css');
 
-	wp_register_style('jcparallax_css', plugins_url('jc-parallax/jcparallax.css', __FILE__));
+	wp_register_style('jcparallax_css', plugins_url('jcparallax/jcparallax.css', __FILE__));
 	wp_enqueue_style('jcparallax_css');
 
     wp_register_style('pospi_admin_base_css', plugins_url('pospi_base_admin.css', __FILE__));
