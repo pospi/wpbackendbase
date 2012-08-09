9eeca509382e8202299a3dd3f29edec5c709911b
pospi <pospi@spadgos.com>
2012-08-09 12:49:03 +1000

    refactored AdminUI::addFilteredListPage() into two methods, one for retrieving a WP_List_Table instance for a post type and one for rendering them

--- admin_ui.class.php
+++ admin_ui.class.php
@@ -28,66 +28,21 @@ abstract class AdminUI
 	 * @param [callable] $queryModifyCb callback hook to handle extra arguments to the WP_Query instance used to display this page's table. This hook is only added on the relevant page.
 	 * @param [string] $pageTitle     	title of the page as displayed in the header
 	 * @param [string] $capability		permission required to access the page
-	 * @param [WP_List_Table] $listTable  if specified, use this custom WP_List_Table instance to render the page instead of one of the builtin ones.
+	 * @param [WP_List_Table] $listTable  if specified, use this custom WP_List_Table instance to render the page instead of one of the builtin one for the post type.
 	 */
 	public static function addFilteredListPage($parentMenu, $menuLabel, $postTypeName, $queryModifyCb, $pageTitle = null, $capability = 'manage_options', WP_List_Table $listTable = null)
 	{
-		AdminMenu::addSubmenu($parentMenu, $menuLabel, function() use ($postTypeName, $queryModifyCb, $pageTitle, $menuLabel)
-		{
-			// store the current screen variable, then set it to one the list understands
-			$resetScreen = false;
-			if (isset($listTable)) {
-				$wp_list_table = $listTable;
-			} else {
-				$resetScreen = get_current_screen();
-				switch ($postTypeName) {
-					case 'attachment':
-						set_current_screen('media');
-						$wp_list_table = _get_list_table('WP_Media_List_Table');	// :IMPORTANT: table must be constructed AFTER screen is set!
-						break;
-					case 'user':
-						set_current_screen('users');
-						$wp_list_table = _get_list_table('WP_Users_List_Table');
-						break;
-					case 'page':
-						set_current_screen('pages');
-						$wp_list_table = _get_list_table('WP_Posts_List_Table');
-						break;
-					default:
-						set_current_screen('edit-' . $postTypeName);
-						$wp_list_table = _get_list_table('WP_Posts_List_Table');
-						break;
-				}
-			}
-			$wp_list_table->prepare_items();
+		if (!isset($listTable)) {
+			$listTable = self::getPostTypeListTable($postTypeName);
+		} else {
+			$listTable->prepare_items();
+		}
 
-			// output the table
-			wp_enqueue_script('inline-edit-post');
-
-			global $wp, $post_type;
-?>
-<h2><?php echo isset($pageTitle) ? $pageTitle : $menuLabel; ?></h2>
-<form id="posts-filter" action="<?php echo add_query_arg( $wp->query_string, '', home_url( $wp->request ) ); ?>" method="post">
-<input type="hidden" name="post_status" class="post_status_page" value="<?php echo !empty($_REQUEST['post_status']) ? esc_attr($_REQUEST['post_status']) : 'all'; ?>" />
-<input type="hidden" name="post_type" class="post_type_page" value="<?php echo $post_type; ?>" />
-<?php
-			$wp_list_table->display();
-?>
-</form>
-<?php
-			if ( $wp_list_table->has_items() ) {
-				$wp_list_table->inline_edit();
-			}
-?>
-<div id="ajax-response"></div>
-<?php
-			// restore the current screen as it was
-			if ($resetScreen) {
-				set_current_screen($resetScreen->id);
-			}
+		AdminMenu::addSubmenu($parentMenu, $menuLabel, function() use ($pageTitle, $menuLabel, $listTable) {
+			echo AdminUI::renderListTablePage($listTable, isset($pageTitle) ? $pageTitle : $menuLabel);
 		});
 
-		// if we're viewing our approval page, add a filter to the query SQL to filter out the approved posts
+		// if we're viewing our page, add the query filter to the request
 		if (isset($_GET['page']) && $_GET['page'] == Custom_Post_Type::get_field_id_name($menuLabel)) {
 			add_filter('request', function($vars) use ($postTypeName, $queryModifyCb) {
 				if ($vars['post_type'] == $postTypeName) {
@@ -142,11 +97,87 @@ abstract class AdminUI
 	}
 
 	//--------------------------------------------------------------------------
+	// UI object helpers
+	//--------------------------------------------------------------------------
+
+	/**
+	 * Retrieves the appropriate WP_List_Table object for the specified post type
+	 * used in drawing that post list in the admin UI.
+	 *
+	 * The list table will come with its items already populated so that the current screen
+	 * can be reset after processing.
+	 *
+	 * @param  string $postTypeName post type (or object type if 'user', 'attachment') to get the table object for
+	 * @return the list table used to draw those posts
+	 */
+	public function getPostTypeListTable($postTypeName)
+	{
+		// set the current screen so we get the correct hooks executed for the table
+		$resetScreen = get_current_screen();
+
+		switch ($postTypeName) {
+			case 'attachment':
+				set_current_screen('media');
+				$wp_list_table = _get_list_table('WP_Media_List_Table');	// :IMPORTANT: table must be constructed AFTER screen is set!
+				break;
+			case 'user':
+				set_current_screen('users');
+				$wp_list_table = _get_list_table('WP_Users_List_Table');
+				break;
+			case 'page':
+				set_current_screen('pages');
+				$wp_list_table = _get_list_table('WP_Posts_List_Table');
+				break;
+			default:
+				set_current_screen('edit-' . $postTypeName);
+				$wp_list_table = _get_list_table('WP_Posts_List_Table');
+				break;
+		}
+		$wp_list_table->prepare_items();
+
+		// restore the current screen as it was
+		if ($resetScreen) {
+			set_current_screen($resetScreen->id);
+		}
+	}
+
+	//--------------------------------------------------------------------------
 	// Screen helpers
 	//--------------------------------------------------------------------------
 
 	/**
+	 * Renders out a list table with all its controls, wrapping form elements and scripts
+	 * :WARNING: this method uses output buffering
+	 */
+	public static function renderListTablePage(WP_List_Table $listTable, $pageTitle)
+	{
+		ob_start();
+		// output the table
+		wp_enqueue_script('inline-edit-post');
+
+		global $wp, $post_type;
+?>
+<h2><?php echo $pageTitle; ?></h2>
+<form id="posts-filter" action="<?php echo add_query_arg( $wp->query_string, '', home_url( $wp->request ) ); ?>" method="post">
+<input type="hidden" name="post_status" class="post_status_page" value="<?php echo !empty($_REQUEST['post_status']) ? esc_attr($_REQUEST['post_status']) : 'all'; ?>" />
+<input type="hidden" name="post_type" class="post_type_page" value="<?php echo $post_type; ?>" />
+<?php
+		$listTable->display();
+?>
+</form>
+<?php
+		if ( $listTable->has_items() ) {
+			$listTable->inline_edit();
+		}
+?>
+<div id="ajax-response"></div>
+<?php
+		return ob_get_clean();
+	}
+
+	/**
 	 * Renders out a taxonomy input.
+	 * :WARNING: this method uses output buffering
 	 */
 	public static function getTaxonomyInput($taxonomyName, $postObj, $hierarchical = false)
 	{
