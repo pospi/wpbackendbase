19b4ae9d70c79115693b6b0e10e901a81d6d7815
pospi <pospi@spadgos.com>
2012-09-09 22:39:07 +1000

    fix twitter user fio-type attribute

--- field_types/formio_field-twitter_user.class.php
+++ field_types/formio_field-twitter_user.class.php
@@ -12,7 +12,7 @@ class FormIOField_Twitter_user extends FormIOField_Text
 		<label for="{$id}">{$desc}{$required? <span class="required">*</span>}</label>
 		<span class="twitter">
 			<a target="_blank" href="https://twitter.com/%s">https://twitter.com/</a>
-			<input type="text" name="{$name}" id="{$id}"{$value? value="$value"}{$readonly? readonly="readonly"} data-fio-type="facebook_user"{$validation? data-fio-validation="$validation"}{$dependencies? data-fio-depends="$dependencies"} />
+			<input type="text" name="{$name}" id="{$id}"{$value? value="$value"}{$readonly? readonly="readonly"} data-fio-type="twitter_user"{$validation? data-fio-validation="$validation"}{$dependencies? data-fio-depends="$dependencies"} />
 		</span>
 		{$error?<p class="err">$error</p>}
 		{$hint? <p class="hint">$hint</p>}
