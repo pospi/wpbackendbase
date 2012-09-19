89105487428cb904691336c189a7d383eaecd434
pospi <pospi@spadgos.com>
2012-09-19 12:19:06 +1000

    remove unneeded WP code from field name generation method

--- custom_post_type.class.php
+++ custom_post_type.class.php
@@ -1319,27 +1319,6 @@ class Custom_Post_Type
 		$title = preg_replace('/&.+?;/', '', $title); // kill entities
 		$title = str_replace('.', '-', $title);
 
-		if ( 'save' == $context ) {
-			// Convert nbsp, ndash and mdash to hyphens
-			$title = str_replace( array( '%c2%a0', '%e2%80%93', '%e2%80%94' ), '-', $title );
-
-			// Strip these characters entirely
-			$title = str_replace( array(
-				// iexcl and iquest
-				'%c2%a1', '%c2%bf',
-				// angle quotes
-				'%c2%ab', '%c2%bb', '%e2%80%b9', '%e2%80%ba',
-				// curly quotes
-				'%e2%80%98', '%e2%80%99', '%e2%80%9c', '%e2%80%9d',
-				'%e2%80%9a', '%e2%80%9b', '%e2%80%9e', '%e2%80%9f',
-				// copy, reg, deg, hellip and trade
-				'%c2%a9', '%c2%ae', '%c2%b0', '%e2%80%a6', '%e2%84%a2',
-			), '', $title );
-
-			// Convert times to x
-			$title = str_replace( '%c3%97', 'x', $title );
-		}
-
 		$title = preg_replace('/[^%a-z0-9 _-]/', '', $title);
 		$title = preg_replace('/\s+/', '_', $title);
 		$title = preg_replace('|-+|', '-', $title);
