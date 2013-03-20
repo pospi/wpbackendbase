56cf27ba41e1f43f84ed410a82a2373f32de5459
pospi <pospi@spadgos.com>
2013-03-20 09:12:37 +0000

    remove confusing rich editor input buttons and line it up with other input types for consistency

--- field_types/formio_field-richedit.class.php
+++ field_types/formio_field-richedit.class.php
@@ -10,7 +10,7 @@ class FormIOField_Richedit extends FormIOField_Text
 {
 	public function __construct($form, $name, $displayText = null, $defaultValue = null)
 	{
-		$this->buildString = '<div class="row{$alt? alt}{$classes? $classes}">
+		$this->buildString = '<div class="row richedit{$alt? alt}{$classes? $classes}">
 			<label for="{$id}">{$desc}{$required? <span class="required">*</span>}</label>
 			{$editor}
 			{$error?<p class="err">$error</p>}
