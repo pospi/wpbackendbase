367c41fe216618a4e31ffd4742ac6b2ba148889f
pospi <pospi@spadgos.com>
2013-04-16 13:00:55 +0100

    refactor admin Form library extensions so that functionality is retained when forms are used on non-post pages

--- pospi_wp_admin.js
+++ pospi_wp_admin.js
@@ -1,207 +1,174 @@
 (function($) {
-	$(function() {
-		// find our metaboxs
-		var metaboxes = $('input[name=custom_post_type]').parent();
-
-		// add img class to formIO image input parents to allow separate styling of actual tokeninput input
-		$('li.img.token-input-token').closest('token-input-list').addClass('img');
-
-		// override form field name retriever to use custom_meta instead of #post or other admin form IDs (which it has to be in wordpress)
-		var oldGetFieldId = FormIO.prototype.getFieldId;
-		FormIO.prototype.getFieldId = function(fldname) {
-			var id = this.elements.get(0).tagName.toLowerCase() == 'form' ? this.elements.attr('id') : this.elements.closest('form').attr('id');
-			if (id == 'post' || id == 'media-single-form' || id == 'file-form' || id == 'addlink' || id == 'editlink' || id == 'createuser' || id == 'your-profile') {
-				return 'custom_meta_' + fldname.replace(/\[/g, '_').replace(/\]/g, '');
-			}
-			return oldGetFieldId.call(this, fldname);
-		};
-		var oldGetFieldName = FormIO.prototype.getFieldName;
-		FormIO.prototype.getFieldName = function(fldId) {
-			var id = this.elements.get(0).tagName.toLowerCase() == 'form' ? this.elements.attr('id') : this.elements.closest('form').attr('id');
-			if (id == 'post' || id == 'media-single-form' || id == 'file-form' || id == 'addlink' || id == 'editlink' || id == 'createuser' || id == 'your-profile') {
-				return 'custom_meta[' + fldId.replace(new RegExp('^custom_meta_'), '')
-							.replace(/_(\d+)_/g, '][$1][') + ']';
-			}
-			return oldGetFieldName.call(this, fldId);
-		};
-
-		// override the form field submit action to indicate an error in the WP UI instead of failing the submission silently
-		var oldOnSubmit = FormIO.prototype.onSubmit;
-		FormIO.prototype.onSubmit = function()
-		{
-			var ok = oldOnSubmit.call(this);
-
-			// remove notifications from previous run
-			$('#wpbody-content > .wrap').find('.formio-notifications').remove();
-
-			if (!ok) {
-				// reset the WP UI to its pre-submission state
-				$('#publish').removeClass('button-primary-disabled');
-				$('#save-post').removeClass('button-disabled');
-				$('#ajax-loading').hide();
-
-				// add notifications of errors
-				var messages = {},
-					messageStr = '',
-					that = this;
-				$.each(this.failedValidators, function(field, validator) {
-					var el = $('#' + field);
-					messages[that.getReadableFieldName(el) + " failed validation."] = true;     // :TODO: validator messages
-				});
-				$.each(messages, function(msg, v) {
-					messageStr += "<div class=\"error formio-notifications\"><p>" + msg + "</p></div>";
-				});
-				var notifications = $(messageStr);
-
-				$('#wpbody-content > .wrap').prepend(notifications);
-			}
-
-			return ok;
-		}
-
-		//--------------------------------------------------------------------------
-		// init form UI javascript
-
-		function initUrlInput(el) {
-			var link = el.prev(),
-				baseHref = link.attr('href');
-
-			link.attr('href', baseHref.replace('%s', el.val()));
-			el.blur(function() {
-				link.attr('href', baseHref.replace('%s', el.val()));
-			});
-		};
-
-		function initPostBoxes(metaboxes) {
-			var postForm, postBoxes;
-
-			if (metaboxes.get(0).tagName.toLowerCase() == 'form') {
-				postBoxes = metaboxes;
-				postForm = null;
-			} else {
-				postForm = metaboxes.closest('form');
-				postBoxes = postForm.find('.postbox.formio .inside');
-			}
-
-			postBoxes.formio({
-				setupRoutines : {
-					// custom (or builtin) post type inputs
-					"[data-fio-type='posttype_attachment']" : function(el) {
-						FormIO.prototype.initAutoCompleteField.call(this, el, {
-							preventDuplicates : true,
-							resultsFormatter : function(item) {
-								return "<li>" + item.name + " <sub>(<a target=\"_blank\" href=\"" + item.editUrl + "\">edit</a> | <a target=\"_blank\" href=\"" + item.viewUrl + "\">view</a>)</sub></li>";
-							},
-							tokenFormatter : function(item) {
-								return "<li><p>" + item.name + " <sub>(<a target=\"_blank\" href=\"" + item.editUrl + "\">edit</a> | <a target=\"_blank\" href=\"" + item.viewUrl + "\">view</a>)</sub></p></li>";
-							}
-						});
-					},
-					"[data-fio-type='posttype_attachment_image']" : function(el) {
-						FormIO.prototype.initAutoCompleteField.call(this, el, {
-							preventDuplicates : true,
-							resultsFormatter : function(item) {
-								return "<li class=\"img\"><img src=\"" + item.thumbUrl + "\" />" + item.name + " <sub>(<a target=\"_blank\" href=\"" + item.editUrl + "\">edit</a> | <a target=\"_blank\" href=\"" + item.viewUrl + "\">view</a>)</sub></li>";
-							},
-							tokenFormatter : function(item) {
-								return "<li class=\"img\"><div><img src=\"" + item.thumbUrl + "\" /></div><p>" + item.name + " <sub>(<a target=\"_blank\" href=\"" + item.editUrl + "\">edit</a> | <a target=\"_blank\" href=\"" + item.viewUrl + "\">view</a>)</sub></p></li>";
-							},
-							// refresh or create parallax image input viewports when added to the input list
-							onAdd : function(item) {
-								var that = this;
-								setTimeout(function() {
-									that.prev('.token-input-list').find('div').each(function(i, imgVp) {
-										var vp;
-										if (vp = $(imgVp).data('jcparallax-viewport')) {
-											vp.refreshCoords();
-										} else {
-											initThumbParallax($(imgVp));
-										}
-									});
-								}, 100);
-							}
-						});
-					},
-					"[data-fio-type='posttype_link']" : function(el) {
-						FormIO.prototype.initAutoCompleteField.call(this, el, {
-							preventDuplicates : true,
-							resultsFormatter : function(item) {
-								return "<li>" + item.name + " <sub>(<a target=\"_blank\" href=\"" + item.editUrl + "\">edit</a> | <a target=\"_blank\" href=\"" + item.linkUrl + "\">open</a>)</sub></li>";
-							},
-							tokenFormatter : function(item) {
-								return "<li><p>" + item.name + " <sub>(<a target=\"_blank\" href=\"" + item.editUrl + "\">edit</a> | <a target=\"_blank\" href=\"" + item.linkUrl + "\">open</a>)</sub></p></li>";
-							}
-						});
-					},
-					"[data-fio-type='posttype_user']" : function(el) {
-						FormIO.prototype.initAutoCompleteField.call(this, el, {
-							preventDuplicates : true,
-							resultsFormatter : function(item) {
-								return "<li>" + item.name + " &lt;" + item.emailAddr + "&gt; <sub>(<a target=\"_blank\" href=\"" + item.editUrl + "\">edit</a>)</sub></li>";
-							},
-							tokenFormatter : function(item) {
-								return "<li><p>" + item.name + " <sub>(<a target=\"_blank\" href=\"" + item.editUrl + "\">edit</a>)</sub></p></li>";
-							}
-						});
-					},
-					"[data-fio-type='plupload']" : function(el) {
-						initUploaderControl(el);
-					},
-
-					".row.external-link" : function(el) {
-						bindLinkDialog(el);
-					},
-
-					// additional useful inputs
-					"[data-fio-type='facebook_user']" : initUrlInput,
-					"[data-fio-type='youtube_user']" : initUrlInput,
-					"[data-fio-type='twitter_user']" : initUrlInput
+	// extend builtin form init routines
+	$.extend(true, FormIO.prototype.options.setupRoutines,
+	{
+		// custom (or builtin) post type inputs
+		"[data-fio-type='posttype_attachment']" : function(el) {
+			FormIO.prototype.initAutoCompleteField.call(this, el, {
+				preventDuplicates : true,
+				resultsFormatter : function(item) {
+					return "<li>" + item.name + " <sub>(<a target=\"_blank\" href=\"" + item.editUrl + "\">edit</a> | <a target=\"_blank\" href=\"" + item.viewUrl + "\">view</a>)</sub></li>";
+				},
+				tokenFormatter : function(item) {
+					return "<li><p>" + item.name + " <sub>(<a target=\"_blank\" href=\"" + item.editUrl + "\">edit</a> | <a target=\"_blank\" href=\"" + item.viewUrl + "\">view</a>)</sub></p></li>";
 				}
 			});
-
-			if (postForm) {
-				postBoxes.formio('setupForm', postForm);
-			}
-		}
-
-		if (metaboxes.get(0)) {
-			initPostBoxes(metaboxes);
-		}
-
-		// load parallax preview of image attachments
-		initThumbParallax($('ul.token-input-list li.img div'));
-
-		//--------------------------------------------------------------------------
-		// custom taxonomy inputs for non-post pages
-
-		function initTagBoxes()
-		{
-			tagBox.init();
-			// save tags on post save/publish
-			$('.tagsdiv').closest('form').submit(function(){
-				$('div.tagsdiv', this).each(function() {
-					tagBox.flushTags(this, false, 1);
-				});
+		},
+		"[data-fio-type='posttype_attachment_image']" : function(el) {
+			FormIO.prototype.initAutoCompleteField.call(this, el, {
+				preventDuplicates : true,
+				resultsFormatter : function(item) {
+					return "<li class=\"img\"><img src=\"" + item.thumbUrl + "\" />" + item.name + " <sub>(<a target=\"_blank\" href=\"" + item.editUrl + "\">edit</a> | <a target=\"_blank\" href=\"" + item.viewUrl + "\">view</a>)</sub></li>";
+				},
+				tokenFormatter : function(item) {
+					return "<li class=\"img\"><div><img src=\"" + item.thumbUrl + "\" /></div><p>" + item.name + " <sub>(<a target=\"_blank\" href=\"" + item.editUrl + "\">edit</a> | <a target=\"_blank\" href=\"" + item.viewUrl + "\">view</a>)</sub></p></li>";
+				},
+				// refresh or create parallax image input viewports when added to the input list
+				onAdd : function(item) {
+					var that = this;
+					setTimeout(function() {
+						that.prev('.token-input-list').find('div').each(function(i, imgVp) {
+							var vp;
+							if (vp = $(imgVp).data('jcparallax-viewport')) {
+								vp.refreshCoords();
+							} else {
+								initThumbParallax($(imgVp));
+							}
+						});
+					}, 100);
+				}
 			});
-		}
+		},
+		"[data-fio-type='posttype_link']" : function(el) {
+			FormIO.prototype.initAutoCompleteField.call(this, el, {
+				preventDuplicates : true,
+				resultsFormatter : function(item) {
+					return "<li>" + item.name + " <sub>(<a target=\"_blank\" href=\"" + item.editUrl + "\">edit</a> | <a target=\"_blank\" href=\"" + item.linkUrl + "\">open</a>)</sub></li>";
+				},
+				tokenFormatter : function(item) {
+					return "<li><p>" + item.name + " <sub>(<a target=\"_blank\" href=\"" + item.editUrl + "\">edit</a> | <a target=\"_blank\" href=\"" + item.linkUrl + "\">open</a>)</sub></p></li>";
+				}
+			});
+		},
+		"[data-fio-type='posttype_user']" : function(el) {
+			FormIO.prototype.initAutoCompleteField.call(this, el, {
+				preventDuplicates : true,
+				resultsFormatter : function(item) {
+					return "<li>" + item.name + " &lt;" + item.emailAddr + "&gt; <sub>(<a target=\"_blank\" href=\"" + item.editUrl + "\">edit</a>)</sub></li>";
+				},
+				tokenFormatter : function(item) {
+					return "<li><p>" + item.name + " <sub>(<a target=\"_blank\" href=\"" + item.editUrl + "\">edit</a>)</sub></p></li>";
+				}
+			});
+		},
+		"[data-fio-type='plupload']" : function(el) {
+			initUploaderControl(el);
+		},
 
-		if ( !$('#tagsdiv-post_tag').length ) {
-			if ($('.tagsdiv').length) {
-				initTagBoxes();
-			} else {
-				$('#side-sortables, #normal-sortables, #advanced-sortables').children('div.postbox').each(function(){
-					if ( this.id.indexOf('tagsdiv-') !== 0 && $(this).hasClass('tagsdiv') ) {
-						initTagBoxes();
-						return false;
-					}
-				});
-			}
-		}
+		".row.external-link" : function(el) {
+			bindLinkDialog(el);
+		},
+
+		// additional useful inputs
+		"[data-fio-type='facebook_user']" : initUrlInput,
+		"[data-fio-type='youtube_user']" : initUrlInput,
+		"[data-fio-type='twitter_user']" : initUrlInput
 	});
 
+	// override form field name retriever to use custom_meta instead of #post or other admin form IDs (which it has to be in wordpress)
+	var oldGetFieldId = FormIO.prototype.getFieldId;
+	FormIO.prototype.getFieldId = function(fldname) {
+		var id = this.elements.get(0).tagName.toLowerCase() == 'form' ? this.elements.attr('id') : this.elements.closest('form').attr('id');
+		if (id == 'post' || id == 'media-single-form' || id == 'file-form' || id == 'addlink' || id == 'editlink' || id == 'createuser' || id == 'your-profile') {
+			return 'custom_meta_' + fldname.replace(/\[/g, '_').replace(/\]/g, '');
+		}
+		return oldGetFieldId.call(this, fldname);
+	};
+	var oldGetFieldName = FormIO.prototype.getFieldName;
+	FormIO.prototype.getFieldName = function(fldId) {
+		var id = this.elements.get(0).tagName.toLowerCase() == 'form' ? this.elements.attr('id') : this.elements.closest('form').attr('id');
+		if (id == 'post' || id == 'media-single-form' || id == 'file-form' || id == 'addlink' || id == 'editlink' || id == 'createuser' || id == 'your-profile') {
+			return 'custom_meta[' + fldId.replace(new RegExp('^custom_meta_'), '')
+						.replace(/_(\d+)_/g, '][$1][') + ']';
+		}
+		return oldGetFieldName.call(this, fldId);
+	};
+
+	// override the form field submit action to indicate an error in the WP UI instead of failing the submission silently
+	var oldOnSubmit = FormIO.prototype.onSubmit;
+	FormIO.prototype.onSubmit = function()
+	{
+		var ok = oldOnSubmit.call(this);
+
+		// remove notifications from previous run
+		$('#wpbody-content > .wrap').find('.formio-notifications').remove();
+
+		if (!ok) {
+			// reset the WP UI to its pre-submission state
+			$('#publish').removeClass('button-primary-disabled');
+			$('#save-post').removeClass('button-disabled');
+			$('#ajax-loading').hide();
+
+			// add notifications of errors
+			var messages = {},
+				messageStr = '',
+				that = this;
+			$.each(this.failedValidators, function(field, validator) {
+				var el = $('#' + field);
+				messages[that.getReadableFieldName(el) + " failed validation."] = true;     // :TODO: validator messages
+			});
+			$.each(messages, function(msg, v) {
+				messageStr += "<div class=\"error formio-notifications\"><p>" + msg + "</p></div>";
+			});
+			var notifications = $(messageStr);
+
+			$('#wpbody-content > .wrap').prepend(notifications);
+		}
+
+		return ok;
+	}
+
 	//--------------------------------------------------------------------------
-	// helpers
-	//--------------------------------------------------------------------------
+	// init helpers
+
+	function initUrlInput(el) {
+		var link = el.prev(),
+			baseHref = link.attr('href');
+
+		link.attr('href', baseHref.replace('%s', el.val()));
+		el.blur(function() {
+			link.attr('href', baseHref.replace('%s', el.val()));
+		});
+	};
+
+	function initPostBoxes(metaboxes) {
+		var postForm, postBoxes;
+
+		if (metaboxes.get(0).tagName.toLowerCase() == 'form') {
+			postBoxes = metaboxes;
+			postForm = null;
+		} else {
+			postForm = metaboxes.closest('form');
+			postBoxes = postForm.find('.postbox.formio .inside');
+		}
+
+		postBoxes.formio();
+
+		if (postForm) {
+			postBoxes.formio('setupForm', postForm);
+		}
+	}
+
+	function initTagBoxes()
+	{
+		tagBox.init();
+		// save tags on post save/publish
+		$('.tagsdiv').closest('form').submit(function(){
+			$('div.tagsdiv', this).each(function() {
+				tagBox.flushTags(this, false, 1);
+			});
+		});
+	}
 
 	function initThumbParallax(els)
 	{
@@ -411,5 +378,40 @@
 			wpActiveEditor = oldActiveEditor;
 		});
 	}
+
+	//--------------------------------------------------------------------------
+	// DOM load
+	//--------------------------------------------------------------------------
+
+	$(function() {
+		// find our metaboxs
+		var metaboxes = $('input[name=custom_post_type]').parent();
+
+		// add img class to formIO image input parents to allow separate styling of actual tokeninput input
+		$('li.img.token-input-token').closest('token-input-list').addClass('img');
+
+		if (metaboxes.get(0)) {
+			initPostBoxes(metaboxes);
+		}
+
+		// load parallax preview of image attachments
+		initThumbParallax($('ul.token-input-list li.img div'));
+
+		//--------------------------------------------------------------------------
+		// custom taxonomy inputs for non-post pages
+
+		if ( !$('#tagsdiv-post_tag').length ) {
+			if ($('.tagsdiv').length) {
+				initTagBoxes();
+			} else {
+				$('#side-sortables, #normal-sortables, #advanced-sortables').children('div.postbox').each(function(){
+					if ( this.id.indexOf('tagsdiv-') !== 0 && $(this).hasClass('tagsdiv') ) {
+						initTagBoxes();
+						return false;
+					}
+				});
+			}
+		}
+	});
 
 })(jQuery);
